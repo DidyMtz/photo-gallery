7ff4b6cf6d3cb8e6be2392bca56c4767f8c2b159
DidyMtz <102537566+DidyMtz@users.noreply.github.com>
2022-04-06 18:32:19 +0000

    save photo

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -7,6 +7,7 @@ import { Storage } from '@capacitor/storage';
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: UserPhoto[] = [];
 
   constructor() { }
 
@@ -18,5 +19,60 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+
+     // Save the picture and add it to photo collection
+  const savedImageFile = await this.savePicture(capturedPhoto);
+  this.photos.unshift(savedImageFile);
   }
+
+
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+  
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+
+
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
 }
